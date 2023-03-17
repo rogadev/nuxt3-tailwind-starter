b756b391e0af2506a54bd4bb8095a7b210489497
Roga <61405141+rogadev@users.noreply.github.com>
2023-03-16 19:10:14 -0700

    Initial commit

---
+++ components/the/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-gray-400 m-5 flex justify-center gap-4">
+    <NuxtLink to="/">
+      <Icon name="heroicons-outline:home" class="w-6 h-6 text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200" />
+    </NuxtLink>
+    <a
+      href="https://github.com/initred/nuxt3-tailwindcss3-starter-kit"
+      target="_blank"
+    >
+      <Icon name="carbon:logo-github" class="w-6 h-6 text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200" />
+    </a>
+    <DarkToggle />
+  </div>
+</template>
